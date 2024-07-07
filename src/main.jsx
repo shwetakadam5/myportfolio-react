7398b976e48e6d1f90984d4ce925e2c46b795df6
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-07-07 18:57:34 +1000

    Initial Changes related to the application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import AboutMe from "./pages/AboutMe.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        // index: true,
+        path: "/",
+        element: <AboutMe />,
+      },
+      {
+        path: "about",
+        // element: <AboutPage />,
+      },
+      {
+        path: "profile/:id",
+        // element: <ProfilePage />,
+      },
+    ],
+  },
+]);
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
