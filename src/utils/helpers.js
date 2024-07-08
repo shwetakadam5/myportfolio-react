e0b6bf559e31845ca299cab8bc9a32c5a0228ca9
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-07-08 16:11:23 +1000

    Initial changes for contact form

---
+++ src/utils/helpers.js
@@ -0,0 +1,13 @@
+export function validateEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+export function checkMessage(input) {
+  const userMessage = /^[A-Za-z]$/;
+  if (input.match(userMessage)) {
+    return true;
+  }
+  return false;
+}
